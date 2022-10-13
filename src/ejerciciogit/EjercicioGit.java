8b035fad90f515dff7cf47919b463b345a99c913
juansinmiedo <juansinmiedo@localhost>
2022-10-13 10:07:32 -0500

    "Add main files"

--- src/ejerciciogit/EjercicioGit.java
+++ src/ejerciciogit/EjercicioGit.java
@@ -16,6 +16,7 @@ public class EjercicioGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Inicio del programa");
     }
     
 }
